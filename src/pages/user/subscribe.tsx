5cde62e5f2ef755506bf50ff698a1fe4af147b40
Mizael Alves <mizaelbna@hotmail.com>
2022-07-08 02:51:05 -0300

    new page

---
+++ src/pages/user/subscribe.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+import Title from "../../components/Header/title";
+
+const SubscribenUser: NextPage = () => {
+  return (
+    <Title title={"Inscreva-se"} subtitle={""}/>
+  )
+};
+
+export default SubscribenUser
