4a09746c7a48290063c2ef59ee519f6c4301ba48
mizaelalves <mizaelbna@hotmail.com>
2022-07-09 23:34:08 -0300

    new infos to header

--- src/components/Header/headerHome.tsx
+++ src/components/Header/headerHome.tsx
@@ -2,14 +2,29 @@ import { HeaderContainer, Logo, LinksContainer } from "./styles/headerHome.style
 import { Link, Button } from "@mui/material";
 import NextLink from "next/link"
 import { useRouter } from "next/router";
+import { useShowUserName } from "../../data/hooks/pages/useShowUserName";
+import { parseCookies } from "nookies";
+
+
 
 export default function HeaderHome() {
+
+  function useVerifyTokenExist(){
+    const {username, verifyTokenExist} = useShowUserName();
+    if(verifyTokenExist() != false){
+      return (
+        <span>Ola, {username}</span>
+      )
+    }
+  }
+  
   const router = useRouter();
+  
   const handleClick = (e: any) => {
     e.preventDefault();
     router.push("/");
   };
-  
+
   return (
     <HeaderContainer>
       <div>
@@ -18,6 +33,7 @@ export default function HeaderHome() {
       <Button variant="contained"><Link component={NextLink} href={"/user/login"}>Login</Link></Button>
        
         <Button variant="contained"><Link component={NextLink} href={"/user/registrar"} >Inscreva-se</Link></Button>
+  
       </LinksContainer>
       </div>
     </HeaderContainer>
